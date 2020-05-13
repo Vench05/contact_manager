862e1e87ed680fe7c2d1e3b587d0cbc8c79869db
Vench05 <ralfvenchbillones@gmail.com>
2020-05-13 17:34:16 +0800

    Updates

---
+++ precache-manifest.0f343f790f1a6558675303160f23df27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e5d89f9dded2462124a6de2d4ab1a0",
+    "url": "/Vench05/contact_manager/index.html"
+  },
+  {
+    "revision": "94bff3aee8c1bca5e5b0",
+    "url": "/Vench05/contact_manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0ed3eb1b3a42e52aba74",
+    "url": "/Vench05/contact_manager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "94bff3aee8c1bca5e5b0",
+    "url": "/Vench05/contact_manager/static/js/2.13d8b200.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Vench05/contact_manager/static/js/2.13d8b200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed3eb1b3a42e52aba74",
+    "url": "/Vench05/contact_manager/static/js/main.6d63a157.chunk.js"
+  },
+  {
+    "revision": "93eef18543ab11cd1ab6",
+    "url": "/Vench05/contact_manager/static/js/runtime-main.c3c41fd9.js"
+  }
+]);
